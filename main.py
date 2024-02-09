4d1a2a4909ef7c350202eb75358c3056260d226f
Adrian Rodriguez <arodcasanova@gmail.com>
2024-02-08 16:53:16 -0800

    Add Student and Teacher stubs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from person import Person
+from teacher import Teacher
 
 if __name__ == "__main__":
+    hcde_teacher = Teacher("Adrian Rodriguez")
+    print(hcde_teacher.name)
---
+++ student.py
@@ -0,0 +1,6 @@
+from person import Person
+
+class Student(Person):
+    def __init__(self, name):
+        super().__init__(name)
+        self.courses_taught = []
