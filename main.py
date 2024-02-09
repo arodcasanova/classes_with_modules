7765aaf8b32e37d8e192886f9b8f5a8697580a4d
Adrian Rodriguez <arodcasanova@gmail.com>
2024-02-08 17:13:25 -0800

    Add Course stub

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from teacher import Teacher
+from course import Course
 
 if __name__ == "__main__":
     hcde_teacher = Teacher("Adrian Rodriguez")
-    print(hcde_teacher.name)
+    hcde_class = Course("HCDE 310", hcde_teacher)
+    print(hcde_class.teacher.name)
+
+    for course in hcde_teacher.courses_taught:
+        print(course.name)
+
+    print(f"Teacher: {hcde_teacher.name} is teaching {hcde_teacher.courses_taught[0].name}")
